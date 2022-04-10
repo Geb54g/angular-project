1883549533ae32a5cba6792a08197f98381825b5
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-11 02:18:43 +0300

    ft:imported quotez component into quote component

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotez } from '../quotez';
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
