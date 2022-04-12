6995db6fc281f7719fa01d56bf4b7b5d601a4773
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-12 08:27:21 +0300

    st:add quotes to the quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Sayings } from '../sayings'
 export class QuoteComponent implements OnInit{
 
     quotes:Sayings[] =[
-      new Sayings(1, 'Quote:', 0, 0),
-      new Sayings(2, 'Quote:' ,0, 0),
-      new Sayings(3, 'Qoute:',0, 0),
-      new Sayings(1, 'Quote:', 0, 0),
-      new Sayings(2, 'Quote:' ,0, 0),
+      new Sayings(1, 'Quote:Everyone knew it was impossibble,until a fool who did not know came along and did it :', 0, 0),
+      new Sayings(2, 'Quote:Believe you can and your are halfway there:' ,0, 0),
+      new Sayings(3, 'Qoute:You attract what you vibrate:',0, 0),
+      new Sayings(1, 'Quote:Be a warrior,not a worrrier:', 0, 0),
+      new Sayings(2, 'Quote:Doubt kills more dreams than failure eve will:' ,0, 0),
     ]
      
 
